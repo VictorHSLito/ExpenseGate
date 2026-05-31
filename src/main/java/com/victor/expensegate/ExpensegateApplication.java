2284015745afdc6563425bca8306e5b52fb6a7d2
Victor <victorferrarihugo@hotmail.com>
2026-05-31 16:42:05 -0300

    Init 1

---
+++ src/main/java/com/victor/expensegate/ExpensegateApplication.java
@@ -0,0 +1,13 @@
+package com.victor.expensegate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensegateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensegateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/expensegate/ExpensegateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.expensegate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensegateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
